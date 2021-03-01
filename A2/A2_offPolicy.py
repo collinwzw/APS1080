772296c097bda3b96b071f2eb4722e8078e53777
Ziwen Wang <zi.w.wang@mail.mcgill.ca>
2021-02-28 21:33:12 -0500

    cartpole on policy

---
+++ A2/A2_offPolicy.py
@@ -0,0 +1,166 @@
+import numpy as np
+import gym
+import scipy
+import scipy.signal
+import matplotlib.pyplot as plt
+from functools import partial
+
+def discretize_val(val, min_val, max_val, num_states):
+    """
+	Discretizes a single float
+	if val < min_val, it gets a discrete value of 0
+	if val >= max_val, it gets a discrete value of num_states-1
+
+	Args:
+	    val (float): value to discretize
+	    min_val (float): lower bound of discretization
+	    max_val (float): upper bound of discretization
+	    num_states (int): number of discrete states
+
+	Returns:
+	    float: discrete value
+	"""
+    state = int(num_states * (val - min_val) / (max_val - min_val))
+    if state >= num_states:
+        state = num_states - 1
+    if state < 0:
+        state = 0
+    return state
+
+def observationToState(num_states, lower_bounds, upper_bounds, obs):
+    """
+	Turns an observation into a discrete state
+
+	Args:
+	    num_states (list): list of number of states for each dimension of observation
+	    lower_bounds (list): list of lowerbounds for discretization
+	    upper_bounds (list): list of upperbounds for discretization
+	    obs (list): observation in R^N to discretize
+
+	Returns:
+	    int: discrete state
+	"""
+    state_idx = []
+    for ob, lower, upper, num in zip(obs, lower_bounds, upper_bounds, num_states):
+        state_idx.append(discretize_val(ob, lower, upper, num))
+
+    return np.ravel_multi_index(state_idx, num_states)
+
+
+
+def EpsilonGreedyPolicy(Q, eps, state):
+    sample = np.random.random_sample()
+
+    num_actions = Q.shape[1]
+
+    if sample > eps:
+        max_val = Q[state, :].max()
+        max_indices = np.where(np.abs(Q[state, :] - max_val) < 1e-5)[0]
+        rand_idx = np.random.randint(len(max_indices))
+        max_action = max_indices[rand_idx]
+
+        return max_action
+    else:
+        return np.random.randint(num_actions)
+
+# Initialize policy
+def initPi(Q, eps):
+    pi = partial(EpsilonGreedyPolicy, Q, eps)
+    return pi
+
+def UpdatePi(Q, eps):
+    pi = partial(EpsilonGreedyPolicy, Q, eps)
+    return pi
+
+def initQ(stateLength, numberOfActions):
+    Q = np.zeros((stateLength, numberOfActions))
+    return Q
+
+def initReturns(stateLength, numberOfActions):
+    returns = {}
+    for i, value in enumerate(Q):
+        returns[i,0] = []
+        returns[i,1] = []
+    return returns
+
+def play(env, policy,stateMapping, display):
+    obs = env.reset()
+    # prev_screen = env.render(mode='rgb_array')
+    # plt.imshow(prev_screen)
+    rewards = []
+    actions = []
+    states = []
+    totalRewards = 0
+    while True:
+        state = stateMapping(obs)
+        action = policy(state)
+        actions.append(action)
+        states.append(state)
+
+        obs, reward, done, info = env.step(action)
+        if display == True:
+            screen = env.render(mode='rgb_array')
+            plt.imshow(screen)
+        totalRewards += reward
+        rewards.append(reward)
+        if done == True:
+            #print(obs)
+            break
+
+
+        #print(totalRewards)
+    return states, actions, rewards
+
+env = gym.make('CartPole-v1')
+
+eps = 0.05
+gamma = 1
+numberOfActions = env.action_space.n
+numberOfStates = [10, 8, 10, 8]
+lowerBounds = [-2.4, -9999, -0.21, -9999]
+upperBounds = [2.4, 9999, 0.21, 9999]
+stateMapping = partial(observationToState, numberOfStates, lowerBounds, upperBounds)
+stateLength = np.prod(np.array(numberOfStates))
+Q = initQ(stateLength, numberOfActions)
+returns = initReturns(stateLength, numberOfActions)
+policy = initPi(Q, eps)
+rewardList = []
+avg = 0
+for i in range(20000):
+    if i%100 == 0 and i != 0:
+        print("i = " + str(i) + ", and average reward over past 100 episode " + str(avg/ 100))
+        avg = 0
+        states, actions, rewards = play(env, policy, stateMapping, False)
+    else:
+        states, actions, rewards = play(env, policy,stateMapping,False)
+
+    seen = set()
+    G = 0
+    for i, (state, action, reward) in enumerate(zip(reversed(states), reversed(actions), reversed(rewards))):
+        if i == 0:
+            G = G * gamma
+        else:
+            G = G * gamma + rewards[i - 1]
+        if state in seen:
+            continue
+
+        seen.add(state)
+        returns[state,action].append(G)
+
+        Q[state, action] = sum(returns[state,action])/ len(returns[state,action])
+        policy = UpdatePi(Q,eps)
+    avg += sum(rewards)
+    rewardList.append(sum(rewards))
+plt.plot(rewardList)
+plt.show()
+eps = 0
+policy = UpdatePi(Q, eps)
+avg = 0
+for i in range(100):
+    states, actions, rewards = play(env, policy, stateMapping, False)
+    avg += sum(rewards)
+print(avg)
+play(env, policy,stateMapping, True)
+
+
+
---
+++ A2/E2.py
@@ -0,0 +1,109 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython import display as ipythondisplay
+from pyvirtualdisplay import Display
+
+
+def getMaximumValueFromDictionary(dictionary):
+    max_key = None
+    max_value = float('-inf')
+    for key, value in dictionary.items():
+        if value > max_value:
+            max_key = key
+            max_value = value
+    return max_key, max_value
+
+def findNearestValue(a, b):
+  index = np.digitize(x=a, bins=b)
+  if index >= len(b):
+      print(index)
+      print(b)
+  return b[index]
+
+
+def getNeastState( obs, states):
+  l_state = []
+  for i in range(4):
+    l_state.append(findNearestValue(obs[i], states[i]))
+  return l_state  # returns the state values of the current observation variable
+
+# Initialize policy
+def initPi(combinations):
+    pi = {}
+
+    #initialize the random policy
+    for state in combinations:
+        key = state.tolist()
+        pi[tuple(key)] = np.random.choice([0,1])
+    return pi
+
+def play(env, policy,v_ndarray,gamma):
+    obs = env.env.state
+    currentState = getNeastState(obs, v_ndarray)
+    action = policy[tuple(currentState)]
+    state_action_reward = [(currentState,action,1)]
+    while True:
+
+        obs, reward, done, info = env.step(action)
+        currentState = getNeastState(obs, v_ndarray)
+        if done == True:
+            state_action_reward.append((currentState,None, 0))
+            break
+        else:
+            action = policy[tuple(currentState)]
+            state_action_reward.append((currentState, action, 1))
+    G = 0
+    state_action_return = []
+    first = True
+
+    for state, action, reward in reversed(state_action_reward):
+        if first:
+            first = False
+        else:
+            state_action_return.append((state,action,G))
+        G = reward + gamma * G
+    state_action_return.reverse()
+    return state_action_return
+
+env = gym.make("CartPole-v0")
+env.reset()
+gamma = 0.95
+
+v_ndarray = [np.linspace(-2.6, 2.6, 10),  np.linspace(-500, 500, 10),
+                       np.linspace(-0.5, 0.5, 10),  np.linspace(-500, 500, 10),]
+mesh = np.array(np.meshgrid(v_ndarray[0], v_ndarray[1],v_ndarray[2],v_ndarray[3]))
+combinations = mesh.T.reshape(-1, 4)
+actionStateValueTable = {}
+R = {}
+for state in combinations:
+  key =  state.tolist()
+  actionStateValueTable[tuple(key)] = {}
+  for action in range(0,2):
+    actionStateValueTable[tuple(key)][action] = 0
+    R[(tuple(key),action)] = []
+
+policy = initPi(combinations)
+
+
+
+for i in range(50000):
+    #action = env.action_space.sample()
+    state_action_return = play(env, policy,v_ndarray,gamma)
+    print("episode " + str(i) )
+    #obs [cart postion, cart velocity, pole angle, pole velocity at tip]
+
+    #calculate Q
+    seen = set()
+    for state, action, G in state_action_return:
+        stateActionPair = (tuple(state),action)
+        if stateActionPair not in seen:
+           R[stateActionPair].append(G)
+           actionStateValueTable[tuple(state)][action] = np.mean(R[stateActionPair])
+           seen.add(stateActionPair)
+
+    #calculate new policy
+    for state in actionStateValueTable.keys():
+        action, _ = getMaximumValueFromDictionary(actionStateValueTable[state])
+        policy[state] = action
+
